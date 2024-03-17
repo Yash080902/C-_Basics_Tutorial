0310002646e8e2d45b990fa3a22ac05b04f818a9
Yash Kumar Goel <goelyash65@gmail.com>
2024-03-17 10:56:41 +0530

    yes we are commited first code of basics

---
+++ basics.cpp/first_basics.cpp
@@ -0,0 +1,13 @@
+// We are using and study about the C++ Basics in this Playlist
+
+#include<iostream> // here we have include the files {header files} iostream ki help <iostream> iski help se COUT & CIN kaam krenge
+using namespace std;  // ye hume ke space dega or identifiers ko space ek limited space
+
+int main()
+{
+    int num1 = 10; // this is the identifier initialization with declaration
+    int num2 = 20;
+
+    cout<<num1+num2<<endl; // this is the cout function which print the value or output the value
+    return 0;  // functio hai to kuch na kuch to return krna hoga 
+} 
